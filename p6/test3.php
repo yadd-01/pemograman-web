8bc1c09ed32f3e43c97728f20c6f843625ea7365
yadd-01 <ariyadi5230@gmail.com>
2025-10-13 19:56:28 +0800

    Add files via upload

---
+++ p6/test3.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <?php
+    echo "<title>Malaman PHP pertamaku</title>";
+    ?>
+</head>
+<body>
+    <hi>Script PHP dalam (X)HTML</hi>
+
+    <?php
+    echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+    echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+    ?>
+    <p>Ini paragraph yang dibuat tanga script PHP</p>
+    <p>Ini paragraph yang dibuat tanga script PHP</p>
+
+    <?php
+    echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+    echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+    ?>
+</body>
+</html>
---
+++ p6/test6.php
@@ -0,0 +1,13 @@
+<?php
+echo "Hello World 1! </br>"; // perintah ini akan menectak Hello World!
+?>
+<?php
+
+echo "Hello World 2! </br>";
+?>
+<?php
+// Berikut ini adalah perintah
+// untuk nenampilkan teks Hello World
+// pada browser
+echo "Hello World 3!";
+?>
